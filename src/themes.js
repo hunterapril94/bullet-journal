266bb58f2e0988d780aa5103a60d7b0992a3b299
April Larsen <hunterapril94@gmail.com>
2023-01-10 10:24:36 -0800

    initial commit

---
+++ src/themes.js
@@ -0,0 +1,33 @@
+export default themes = {
+  "Rosettes and Cream" : [
+      '#EF7C8E',
+      '#FAE8E0',
+      '#B6E2D3',
+      '#D8A7B1'
+    ],
+  "Summer Splash" : [
+    '#05445E',
+    '#189AB4',
+    '#75E6DA',
+    '#D4F1F4'
+  ],
+  "Retro Punch" : [
+    '#2FF3E0',
+    '#F8D210',
+    '#FA26A0',
+    '#F51720'
+  ],
+  "Afternoon Sinkers" : [
+    '#FFC2C7',
+    '#B6E5D8',
+    '#FBE5C8',
+    '#8FDDE7'
+  ],
+  "Easter Egg Nest" : [
+    '#BBE7FE',
+    '#D3B5E5',
+    '#FFD4DB',
+    '#EFF1DB'
+  ]
+}
+
